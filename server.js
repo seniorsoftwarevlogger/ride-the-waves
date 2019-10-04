41855bdb0b3f2c6c8fad2384d04b770e88d9f502
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2019-10-04 08:45:32 +0200

    wip: server

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const https = require("https");
+
+const { bloggersIds } = require("./data");
+
+const { YOUTUBE_API } = process.env;
 
 const app = express();
 
@@ -6,6 +11,39 @@ app.use(express.static(__dirname + "/dist"));
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
+});
+
+app.get("/api/bloggers", (req, res) => {
+  const ids = bloggersIds.join(",");
+
+  https
+    .get(
+      `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${ids}&fields=items(id%2Csnippet%2Fthumbnails%2Csnippet%2Ftitle)&key=${YOUTUBE_API}`,
+      apiRes => {
+        apiRes.setEncoding("utf8");
+
+        let data;
+
+        // wait for data
+        apiRes.on("data", function(chunk) {
+          res.write(chunk);
+          data += chunk;
+        });
+
+        apiRes.on("close", function() {
+          res.end();
+        });
+
+        apiRes.on("end", function() {
+          res.end();
+        });
+      }
+    )
+    .on("error", function(e) {
+      console.log(e.message);
+      res.writeHead(500);
+      res.end();
+    });
 });
 
 let port = process.env.PORT || 5000;
