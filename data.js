d4ffb1d4edbcffc85d52c2ed88c5239be856d70a
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2019-10-04 08:46:04 +0200

    wip: data

---
+++ data.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bloggersIds: [
+    "UCVswRUcKC-M35RzgPRv8qUg",
+    "UCX3w3jB05SHLbGjZPR0PM6g",
+    "UC7f5bVxWsm3jlZIPDzOMcAg",
+    "UCeObZv89Stb2xLtjLJ0De3Q",
+    "UCn5wduCq2Mus0v85QZn9IaA",
+    "UC_hvS-IJ_SY04Op14v3l4Lg",
+    "UCZavnENoyHObZlJrXKLvOEQ"
+  ]
+};
+
