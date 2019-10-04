e6c22eb56f6e909d8ac227577b4fdd731bc734f5
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2019-10-04 08:44:54 +0200

    wip: app

--- src/components/app.js
+++ src/components/app.js
@@ -4,34 +4,89 @@ import React from "react";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      bloggersList: []
+    };
     this.authFunc = this.authFunc.bind(this);
   }
+
+  componentWillMount() {
+    fetch(`/api/bloggers`)
+      .then(res => res.json())
+      .then(body => {
+        this.setState({ bloggersList: body.items });
+      });
+  }
+
   authFunc() {
-    const authData = { data: "Auth on my site" };
-    if (WavesKeeper) {
-      WavesKeeper.auth(authData)
+    const authData = { data: "IT Bloggers Contest" };
+    if (window.WavesKeeper) {
+      window.WavesKeeper.auth(authData)
         .then(auth => {
-          console.log(auth); //displaying the result on the console
-          /*...processing data */
+          console.log(auth);
         })
         .catch(error => {
-          console.error(error); // displaying the result on the console
-          /*...processing errors */
+          console.error(error);
         });
     } else {
-      alert("To Auth WavesKeeper should be installed.");
+      alert("Пожалуйста, установите WavesKeeper.");
     }
   }
+
+  handleVote(bloggerId) {}
+  handleReset() {}
+
   render() {
     return (
       <div className="container">
-        <input
-          className="btn btn-primary"
-          type="submit"
-          value="Auth"
-          onClick={this.authFunc}
-        />
+        <div className="jumbotron mt-4">
+          <h1 className="display-4">Лучший АйТи блоггер!</h1>
+          <p className="lead">
+            Голосуй за лучшего айти блоггера! Победитель не получит ничего!
+          </p>
+          <hr className="my-4" />
+          <p>
+            Голосование открытое на блокчейн платформе Waves. Чтобы голосовать
+            установи{" "}
+            <a href="https://docs.wavesplatform.com/en/waves-keeper/about-waves-keeper.html">
+              WaveKeeper
+            </a>
+            .
+          </p>
+          <p className="lead">
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={this.authFunc}
+            >
+              Авторизовать WaveKeeper
+            </button>
+          </p>
+        </div>
+
+        <ul className="list-unstyled">
+          {this.state.bloggersList.map(blogger => (
+            <li className="media my-3">
+              <img
+                className="mr-3 img-thumbnail"
+                src={blogger.snippet.thumbnails.default.url}
+              />
+              <div className="media-body">
+                <h5 className="mt-0 mb-2">
+                  <a href={`https://www.youtube.com/channel/${blogger.id}`}>
+                    {blogger.snippet.title}
+                  </a>
+                </h5>
+                <input
+                  className="btn btn-light"
+                  type="button"
+                  value="Голосовать!"
+                  onClick={this.handleVote.bind(null, blogger.id)}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
